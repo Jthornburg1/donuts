66efbb93638cd7dfc7b36d1bb6a54313a8fe32c3
Jon Thornburg <jon_thornburg@clickherelabs.com>
2017-04-10 13:15:39 -0500

    adds new reachability variable

---
+++ Reachability.swift
@@ -0,0 +1,62 @@
+//
+//  Reachability.swift
+//  towersOfHanoi
+//
+//  Created by Jon Thornburg on 4/10/17.
+//  Copyright © 2017 Jon Thornburg. All rights reserved.
+//
+
+import Foundation
+import SystemConfiguration
+
+protocol Utilities {
+}
+
+extension NSObject:Utilities {
+    
+    enum ReachabilityStatus {
+        case notReachable
+        case reachableViaWWAN
+        case reachableViaWifi
+    }
+    
+    var currentReachabilityStatus: ReachabilityStatus {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout<sockaddr_in>.size)
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress, {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                SCNetworkReachabilityCreateWithAddress(nil, $0)
+            }
+        }) else {
+            return .notReachable
+        }
+        
+        var flags: SCNetworkReachabilityFlags = []
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) {
+            return .notReachable
+        }
+        
+        if flags.contains(.reachable) == false {
+            // The target host is not reachable.
+            return .notReachable
+        }
+        else if flags.contains(.isWWAN) == true {
+            // WWAN connections are OK if the calling application is using the CFNetwork APIs.
+            return .reachableViaWWAN
+        }
+        else if flags.contains(.connectionRequired) == false {
+            // If the target host is reachable and no connection is required then we'll assume that you're on Wi-Fi...
+            return .reachableViaWifi
+        }
+        else if (flags.contains(.connectionOnDemand) == true || flags.contains(.connectionOnTraffic) == true) && flags.contains(.interventionRequired) == false {
+            // The connection is on-demand (or on-traffic) if the calling application is using the CFSocketStream or higher APIs and no [user] intervention is needed
+            return .reachableViaWifi
+        }
+        else {
+            return .notReachable
+        }
+
+    }
+}
